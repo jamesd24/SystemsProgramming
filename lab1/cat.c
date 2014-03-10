79b8206d60658f127b8e91baca63baaca8d30f0c
Phil <phil.ward2@hotmail.co.uk>
2014-03-10 21:30:48 +1000

    Added cat to the repo

---
+++ lab1/cat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argsc, char *argsv[])
+{
+	int i;
+	char fcont;
+	FILE *file;
+
+	for(i = 1; i < argsc; i++)
+	{
+		file = fopen(argsv[i], "r");
+		if(file == NULL)
+		{
+			printf("%s\n", argsv[i]);			
+		}
+		else
+		{			
+			while((fcont = fgetc(file)) != EOF)
+			{				
+				printf("%c", fcont);								
+			}
+			printf("\n");			
+		}
+	}
+	
+	return 0;
+}
